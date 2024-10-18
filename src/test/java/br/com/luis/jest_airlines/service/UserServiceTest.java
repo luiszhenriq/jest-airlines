1435655f9d2d899555a0fb9bdd33b85287c28613
Luis Henrique <luishenrique2859@hotmail.com>
2024-10-18 15:45:03 -0300

    feat: refactoring class and create exception find by id test

--- src/test/java/br/com/luis/jest_airlines/service/UserServiceTest.java
+++ src/test/java/br/com/luis/jest_airlines/service/UserServiceTest.java
@@ -1,27 +1,35 @@
 package br.com.luis.jest_airlines.service;
 
 import br.com.luis.jest_airlines.dto.user.UserResponseDTO;
+import br.com.luis.jest_airlines.infra.exception.IdNotFoundException;
 import br.com.luis.jest_airlines.model.User;
 import br.com.luis.jest_airlines.repositories.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    public static final UUID ID =               UUID.randomUUID();
+    public static final String FULL_NAME =      "Luis Henrique";
+    public static final String EMAIL =          "luishenrique@gmail.com";
+    public static final String PASSWORD =       "123456";
+    public static final LocalDate BIRTH =        LocalDate.of(2004, 6, 16);
+    public static final String CPF =            "123243535";
+    public static final String PHONE_NUMBER =   "71941413132";
 
     @InjectMocks
     private UserService service;
@@ -29,21 +37,36 @@ class UserServiceTest {
     @Mock
     private UserRepository repository;
 
+    private User user;
+
+    @BeforeEach
+    void startUser() {
+        user = new User(ID, FULL_NAME, EMAIL, PASSWORD, BIRTH, CPF, PHONE_NUMBER);
+    }
 
     @Test
     @DisplayName("Should Return A User By Id")
     void shouldReturnAUserById() {
-        User newUser = new User(UUID.randomUUID(), "Luis Henrique", "luishenrique@gmail.com",
-                "123456", LocalDate.of(2004, 6, 16), "123243535",
-                "71941413132");
+        when(repository.findById(ID)).thenReturn(Optional.of(user));
 
-        Mockito.when(repository.findById(newUser.getId())).thenReturn(Optional.of(newUser));
+        UserResponseDTO response = service.findById(ID);
 
-        UserResponseDTO response = service.findById(newUser.getId());
+        assertNotNull(response);
+        assertEquals(UserResponseDTO.class, response.getClass());
+        assertEquals(ID, response.id());
+    }
 
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(UserResponseDTO.class, response.getClass());
-        Assertions.assertEquals(newUser.getId(), response.id());
+    @Test
+    @DisplayName("Should return an id not found exception")
+    void shouldReturnAnIdNotFoundException() {
+        when(repository.findById(ID)).thenThrow(new IdNotFoundException("Usuário não encontrado"));
+
+        try {
+            service.findById(ID);
+        }catch (Exception ex) {
+            assertEquals(IdNotFoundException.class, ex.getClass());
+            assertEquals("Usuário não encontrado", ex.getMessage());
+        }
     }
 
 }
